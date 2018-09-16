67e39254c5e055cfab051313f56fe77551ec70b8
Yann001 <1156203992@qq.com>
2018-09-16 12:05:36 +0800

    for merge

---
+++ Assets/Scripts/Game/Test.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour {
+
+    public string TestMethod()
+    {
+        Debug.Log("I'm from TestMethod");
+        return "Yeah!!!";
+    }
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
